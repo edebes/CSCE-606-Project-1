92c24a60299beada05c717696d3778b1e61a63ff
Eric <eric.debes@tamu.edu>
2026-09-14 11:13:41 -0500

    Added more tests

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,3 +5,17 @@ RSpec.describe Word do
     expect(Word.create(text: "test")) == Word.get("test")
   end
 end
+
+RSpec.describe Word do
+  it 'updates a word' do
+    word = Word.create(text: "test")
+    expect(word.update(text: "updated")) == Word.get("updated")
+  end
+end
+
+RSpec.describe Word do
+  it 'deletes a word' do
+    word = Word.create(text: "test")
+    expect(word.delete) == true
+  end
+end
