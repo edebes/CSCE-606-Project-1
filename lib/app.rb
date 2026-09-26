5ec1fa473ef805ca67e97a353f7d38ceebee0429
Eric <eric.debes@tamu.edu>
2026-09-26 13:42:41 -0500

    Updated manage_words.rb with database logic

--- lib/app.rb
+++ lib/app.rb
@@ -4,19 +4,9 @@ require 'sequel'
 require 'json'
 require_relative 'leaderboard'
 require_relative 'typing_session.rb'
-require_relative 'manage_word.rb'
+require_relative 'manage_words.rb'
 
-DB = Sequel.connect('sqlite://api.db')
-DB.create_table? :words do
-  primary_key :id
-  String :text, null: false
-end
-
-class Word < Sequel::Model(:words)
-    plugin :json_serializer
-end
-
-class WordCLI
+class AppCLI
     def self.run
         puts "\nWelcome to the Wordbank!"
         loop do
@@ -31,7 +21,7 @@ class WordCLI
                 TypingSession.practice_typing
             when 2
                 # go to database menu to manage words
-                self.manage_words
+                WordCLI.manage_words
             when 3
                 #leaderboard call
             when 4
@@ -41,109 +31,8 @@ class WordCLI
             end
         end
     end
-
-    def self.manage_words
-        puts "\nWelcome to the Wordbank!"
-        loop do
-            puts "1. List all words"
-            puts "2. Add a word"
-            puts "3. Update a word"
-            puts "4. Delete a word"
-            puts "5. Delete all words"
-            puts "6. Go Back\n"
-            choice = gets.chomp.to_i
-            case choice
-            when 1
-                self.list_words
-            when 2
-                self.add_word
-            when 3
-                self.update_word
-            when 4
-                self.delete_word
-            when 5
-                self.delete_all_words
-            when 6
-                #go back to above menu
-                break
-            else
-                puts "Invalid option. Please try again."
-            end
-        end
-            
-    end
-
-    def self.list_words
-        words = Word.all
-        if words.empty?
-            puts "\nNo words in wordbank."
-        else
-            puts "\nWords:"
-            words.each { |word|
-                puts "#{word.text}"
-            }
-        end
-    end
-
-    def self.add_word
-        puts "\nEnter new word:"
-        text = gets.chomp
-        if text[/\A[a-zA-Z]+\z/] != text
-            puts "\nInvalid word. Please enter a word with only letters."
-            return
-        end
-        word = Word.create(text: text)
-        puts "\nWord added: #{word.text}"
-    end
-
-    def self.update_word
-        if Word.count == 0
-            puts "\nNo words in wordbank to update."
-            return
-        end
-        puts "\nEnter the word to update:"
-        old_text = gets.chomp
-        word = Word.find(text: old_text)
-        if word
-            puts "\nEnter the updated word:"
-            new_text = gets.chomp
-            word.update(text: new_text)
-            puts "\nWord updated from #{old_text} to #{new_text}"
-        else
-            puts "\nWord not found."
-        end
-    end
-
-    def self.delete_word
-        if Word.count == 0
-            puts "\nNo words in wordbank to delete."
-            return
-        end
-        puts "\nEnter the word to delete:"
-        text = gets.chomp
-        word = Word.find(text: text)
-        if word
-            word.delete
-            puts "\nWord deleted: #{word.text}"
-        else
-            puts "\nWord not found."
-        end
-    end
-
-    def self.delete_all_words
-        if Word.count == 0
-            puts "\nNo words in wordbank to delete."
-            return
-        end
-        puts "\nAre you sure you want to delete all words? (y/n)"
-        ans = gets.chomp.downcase
-        if ans == 'y'
-            Word.dataset.delete
-            puts "\nAll words deleted."
-        end
-    end
 end
 
 if __FILE__ == $PROGRAM_NAME
-    WordCLI.run
+    AppCLI.run
 end
---
+++ lib/manage_words.rb
@@ -0,0 +1,117 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'sequel'
+require 'json'
+
+DB = Sequel.connect('sqlite://api.db')
+DB.create_table? :words do
+  primary_key :id
+  String :text, null: false
+end
+
+class Word < Sequel::Model(:words)
+    plugin :json_serializer
+end
+
+class WordCLI
+    def self.manage_words
+        puts "\nWelcome to the Wordbank!"
+        loop do
+            puts "1. List all words"
+            puts "2. Add a word"
+            puts "3. Update a word"
+            puts "4. Delete a word"
+            puts "5. Delete all words"
+            puts "6. Go Back\n"
+            choice = gets.chomp.to_i
+            case choice
+            when 1
+                self.list_words
+            when 2
+                self.add_word
+            when 3
+                self.update_word
+            when 4
+                self.delete_word
+            when 5
+                self.delete_all_words
+            when 6
+                #go back to above menu
+                break
+            else
+                puts "Invalid option. Please try again."
+            end
+        end  
+    end
+    
+    def self.list_words
+        words = Word.all
+        if words.empty?
+            puts "\nNo words in wordbank."
+        else
+            puts "\nWords:"
+            words.each { |word|
+                puts "#{word.text}"
+            }
+        end
+    end
+
+    def self.add_word
+        puts "\nEnter new word:"
+        text = gets.chomp
+        if text[/\A[a-zA-Z]+\z/] != text
+            puts "\nInvalid word. Please enter a word with only letters."
+            return
+        end
+        word = Word.create(text: text)
+        puts "\nWord added: #{word.text}"
+    end
+
+    def self.update_word
+        if Word.count == 0
+            puts "\nNo words in wordbank to update."
+            return
+        end
+        puts "\nEnter the word to update:"
+        old_text = gets.chomp
+        word = Word.find(text: old_text)
+        if word
+            puts "\nEnter the updated word:"
+            new_text = gets.chomp
+            word.update(text: new_text)
+            puts "\nWord updated from #{old_text} to #{new_text}"
+        else
+            puts "\nWord not found."
+        end
+    end
+
+    def self.delete_word
+        if Word.count == 0
+            puts "\nNo words in wordbank to delete."
+            return
+        end
+        puts "\nEnter the word to delete:"
+        text = gets.chomp
+        word = Word.find(text: text)
+        if word
+            word.delete
+            puts "\nWord deleted: #{word.text}"
+        else
+            puts "\nWord not found."
+        end
+    end
+
+    def self.delete_all_words
+        if Word.count == 0
+            puts "\nNo words in wordbank to delete."
+            return
+        end
+        puts "\nAre you sure you want to delete all words? (y/n)"
+        ans = gets.chomp.downcase
+        if ans == 'y'
+            Word.dataset.delete
+            puts "\nAll words deleted."
+        end
+    end
+end
+
